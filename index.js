da91275268a4c57ac72e4cd629cfe0bc589e0b2a
Yash Solanki <solankiyash556@gmail.com>
2023-03-02 17:13:15 +0530

    changed handleSlider() formula

--- index.js
+++ index.js
@@ -20,9 +20,7 @@ setIndicator('#ccc')
 function handleSlinder() {
     inputSlider.value = passwordLength
     lengthDisplay.innerText = passwordLength;
-    const min = inputSlider.min;
-    const max = inputSlider.max;
-    inputSlider.style.backgroundSize = ((passwordLength - min)*100/(max-min)) + "% 100%";
+    inputSlider.style.backgroundSize = ((passwordLength/inputSlider.max)*100) + "% 100%";
 }
 
 function setIndicator(color) {
